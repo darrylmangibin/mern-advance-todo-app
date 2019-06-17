cd67ec91411f3c5f28b5944c2b34c66b092641f6
Darryl <darrylmangibin@gmail.com>
2019-06-17 22:47:27 +0800

    [UPDATED] route GET added

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+// MODEL
+const Todo = require('../../models/Todos');
+
+// @ROUTE GET
+// @desc Get all todos
+// @access Public
+router.get('/', (req, res) => {
+  Todo.find().then((todos) => {
+    res.status(200).json(todos)
+  }).catch((err) => {
+    res.status(400).json({ success: false })
+  })
+})
+
 module.exports = router;
