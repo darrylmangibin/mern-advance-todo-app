a58164f59e4c1767e06fd5b5775f52f243958eb4
Darryl <darrylmangibin@gmail.com>
2019-06-17 22:52:04 +0800

    [UPDATED] route POST added

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -15,4 +15,19 @@ router.get('/', (req, res) => {
   })
 })
 
+// @ROUTE POST
+// @desc Create a todos
+// @access Public
+router.post('/', (req, res) => {
+  const { name } =req.body;
+  const newItem = new Todo({
+    name
+  });
+  newItem.save().then((todo) => {
+    res.status(200).json({ success: true, todo })
+  }).catch((err) => {
+    res.status(400).json({ success: false, msg: 'Unable to save the data' })
+  })
+})
+
 module.exports = router;
