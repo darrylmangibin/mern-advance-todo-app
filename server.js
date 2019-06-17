667e9429d5c27e6b80d6381283bc948ee7dabd63
Darryl <darrylmangibin@gmail.com>
2019-06-17 22:37:16 +0800

    MongoDB initialize

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+// Initialize express
 const app = express();
+
+// MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// MongoDB initialize
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true }).then(() => {
+  console.log('MongoDB Connected')
+}).catch(() => {
+  console.log('Connection failed')
+})
 
 // PORT
 const port = process.env.PORT || 5000;
