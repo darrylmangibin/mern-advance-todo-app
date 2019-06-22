6b76389a5c48fa8c808a3da0c337b4875c0add13
Darryl <darrylmangibin@gmail.com>
2019-06-22 21:20:40 +0800

    router initialize

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// DB CONFIG
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true }).then(() => {
+  console.log(`MongoDB connected`)
+}).catch(() => {
+  console.log('Connection failed')
+});
+
+// ROUTES
+const todos = require('./routes/api/todos');
+app.use('/api/todos', todos);
 
 // PORT
 const port = process.env.PORT || 5000;
