31fc3e9f171f183006725ee4dabd8a13bf9c74c2
Darryl <darrylmangibin@gmail.com>
2019-06-22 21:31:15 +0800

    [UPDATED] route GET method

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+// MODELS
+const Todos = require('../../models/Todos');
+
+
+// @ROUTE GET
+// @desc Get all todos
+// @access Public
 router.get('/', (req, res) => {
-  res.send('test')
+  Todos.find().then((todos) => {
+    res.json(todos)
+  }).catch((err) => {
+    console.log(err)
+    res.status(400).json({
+      success: false,
+      message: 'Could not get the data'
+    })
+  })
 })
 
 module.exports = router;
