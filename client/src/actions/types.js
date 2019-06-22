f234498af158deda3bf115412b5d7e019e203148
Darryl <darrylmangibin@gmail.com>
2019-06-22 21:13:09 +0800

    server initialize

--- client/src/actions/types.js
+++
@@ -1,6 +0,0 @@
-export const GET_TODOS = 'GET_TODOS';
-export const REMOVE_TODO = 'REMOVE_TODO';
-export const ADD_TODO = 'ADD_TODO';
-export const CHECK_TODO = 'CHECK_TODO';
-export const FILTER_TODOS = 'FILTER_TODOS';
-export const HIDE_TODOS = 'HIDE_TODOS';
--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
-
-// MIDDLEWARE
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.options('*', cors());
-
-// MongoDB
-const db = require('./config/keys').mongoURI;
-mongoose.connect(db, { useNewUrlParser: true }).then(() => {
-  console.log(`MongoDB Connected`)
-}).catch(() => {
-  console.log('Connection failed')
-})
-
-// ROUTE
-const todos = require('./routes/api/todos');
-app.use('/api/todos', todos);
 
 // PORT
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running at port: ${port}`)
-})
+});
